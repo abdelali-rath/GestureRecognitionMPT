61e75c99330da5048a20b03b55c2003ed5b605df
Luan Jasari <luan.jasari1@hotmail.com>
2026-06-08 10:29:07 +0200

    Dataset<. 25 saubere Aufnahmen für den Buchstaben Y hinzugefügt

--- GestureRecognition/modules/preprocessor.py
+++ GestureRecognition/modules/preprocessor.py
@@ -25,7 +25,7 @@ class Preprocessor(Module):
         # Konfiguration sicher laden (wie beim TrailMarker)
         config = data.get("config", {}).get("preprocessor", {})
         
-        self.label = config.get("label", "S")
+        self.label = config.get("label", "Y")
         self.dataset_path = config.get("dataset_path", "dataset")
 
 # Klassenordner erstellen
