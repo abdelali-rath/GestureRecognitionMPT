31682dfc35e1d35d60762805fa02da334d5ea017
Luan Jasari <luan.jasari1@hotmail.com>
2026-06-13 10:27:11 +0200

    neue Aufnahmen für die Buchstaben L und K hinzugefügt

--- GestureRecognition/modules/preprocessor.py
+++ GestureRecognition/modules/preprocessor.py
@@ -25,7 +25,7 @@ class Preprocessor(Module):
         # Konfiguration sicher laden (wie beim TrailMarker)
         config = data.get("config", {}).get("preprocessor", {})
         
-        self.label = config.get("label", "T")
+        self.label = config.get("label", "K")
         self.dataset_path = config.get("dataset_path", "dataset")
 
 # Klassenordner erstellen
