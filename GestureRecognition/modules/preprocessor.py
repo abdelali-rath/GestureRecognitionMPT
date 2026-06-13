7af3f564bb3ff4b16392dcfc452f2685928180a6
Luan Jasari <luan.jasari1@hotmail.com>
2026-06-13 11:44:13 +0200

    neue Aufnahmen für den Buchstaben Q hinzugefügt

--- GestureRecognition/modules/preprocessor.py
+++ GestureRecognition/modules/preprocessor.py
@@ -25,7 +25,7 @@ class Preprocessor(Module):
         # Konfiguration sicher laden (wie beim TrailMarker)
         config = data.get("config", {}).get("preprocessor", {})
         
-        self.label = config.get("label", "H")
+        self.label = config.get("label", "Q")
         self.dataset_path = config.get("dataset_path", "dataset")
 
 # Klassenordner erstellen
