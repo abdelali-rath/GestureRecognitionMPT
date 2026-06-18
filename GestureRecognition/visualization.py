56ec2c4f3b172f092b4d2e0b495cebf28d4aa1c8
Luan Jasari <luan.jasari1@hotmail.com>
2026-06-18 21:27:56 +0200

    Aufnahmen für I hinzugefügt

--- GestureRecognition/visualization.py
+++ GestureRecognition/visualization.py
@@ -54,4 +54,4 @@ def replay_recordings(base_path="dataset", label="A"):
 # ==========================================
 if __name__ == "__main__":
     print("Starte Datenexploration...")
-    visualize_dataset(label="J", start=0, stop=21)
+    visualize_dataset(label="I", start=11, stop=20)
