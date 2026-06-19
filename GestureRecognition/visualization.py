6f59c1b505bf752860e52ec7d2448c36d9d65f95
Luan Jasari <luan.jasari1@hotmail.com>
2026-06-19 11:34:18 +0200

    Neue Aufnahmen für Q hinzugefügt

--- GestureRecognition/visualization.py
+++ GestureRecognition/visualization.py
@@ -54,4 +54,4 @@ def replay_recordings(base_path="dataset", label="I"):
 # ==========================================
 if __name__ == "__main__":
     print("Starte Datenexploration...")
-    visualize_dataset(label="I", start=0, stop=4)
+    visualize_dataset(label="X", start=0, stop=4)
